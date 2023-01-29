910c2b76ab11c8e3bef738abaf2033fbb8a7af92
Aymeric <aymeric.pilaert@hotmail.fr>
2023-01-29 14:41:45 +0100

    Initial Commit

---
+++ AlarmiOSApp/AlarmiOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmiOSAppApp.swift
+//  AlarmiOSApp
+//
+//  Created by Aymeric Pilaert on 29/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct AlarmiOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
