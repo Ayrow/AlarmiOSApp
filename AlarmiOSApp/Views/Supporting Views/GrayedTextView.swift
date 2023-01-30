b8e498c88ab21c710e9f1d64448b7c9c1bc0a1ad
Aymeric <aymeric.pilaert@hotmail.fr>
2023-01-30 09:26:43 +0100

    setup folder structure

---
+++ AlarmiOSApp/Views/Supporting Views/GrayedTextView.swift
@@ -0,0 +1,19 @@
+
+import SwiftUI
+
+struct GrayedTextView: View {
+    let text: LocalizedStringKey
+    var font = Font.headline
+    
+    var body: some View {
+        Text(text)
+            .foregroundColor(.gray)
+            .font(font)
+    }
+}
+
+struct GrayedTextView_Previews: PreviewProvider {
+    static var previews: some View {
+        GrayedTextView(text: "Hello")
+    }
+}
