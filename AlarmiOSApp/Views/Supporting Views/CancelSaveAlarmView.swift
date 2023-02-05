2eb1d1b576828fc2300136b3032f740b884053a3
Aymeric <aymeric.pilaert@hotmail.fr>
2023-02-05 11:16:47 +0100

    Setup CancelSaveAlarmView

---
+++ AlarmiOSApp/Views/Supporting Views/CancelSaveAlarmView.swift
@@ -0,0 +1,45 @@
+//
+//  CancelSaveAlarmView.swift
+//  AlarmiOSApp
+//
+//  Created by Aymeric Pilaert on 05/02/2023.
+//
+
+import SwiftUI
+
+struct CancelSaveAlarmView: View {
+    let currentAlarmIndex: Int?
+    @Binding var alarmModel: AlarmModel
+    
+    var body: some View {
+        HStack {
+            // Cancel button
+            Button {
+                // Cancel todo
+            } label: {
+                Text("Cancel")
+            }
+            
+            Spacer()
+            
+            Button {
+                // Save todo
+                if let currentAlarmIndex = currentAlarmIndex {
+                    // TODO: Edit alarm to view model
+                } else {
+                    // TODO: Append alarm to view model
+                }
+            } label: {
+                Text("Save")
+            }
+
+            
+        }
+    }
+}
+
+struct CancelSaveAlarmView_Previews: PreviewProvider {
+    static var previews: some View {
+        CancelSaveAlarmView(currentAlarmIndex: nil, alarmModel: .constant(.DefaultAlarm()))
+    }
+}
